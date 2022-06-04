cee88676027329882931916602d60173909b1987
R0drigu3svi <viiirodriguescanal@gmail.com>
2022-06-04 14:43:24 -0300

    Update exercicio 3 - semana 7 - exercícios obrigatórios

--- 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercicio3/script.js
+++ 04_AUT_EST_EX_OBRIGATORIOS/Semana 7/Exercicio3/script.js
@@ -1,4 +1,9 @@
+var numero = String(document.querySelector("#numero").value);
+var sequencia = numero.split('');
 function checar() {
-    var numero = document.querySelector("#num").value
-    if (numero != [])
+    if (sequencia[0] == "(" && sequencia[3] == ")" && sequencia[9] == "-") {
+        alert("Parabéns, número verificado.");
+    } else {
+        alert("Tente novamente! O formato não esta sendo respeitado.");
+    }
 }
